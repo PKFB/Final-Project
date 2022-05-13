14cbad117f19bfecfc46bd8eaa68ba5426430a4d
melissakavege <mkavege53@gmail.com>
2022-05-12 22:32:41 -0400

    Update book_recommender_test.py

--- book_recommender_test.py
+++ book_recommender_test.py
@@ -1,8 +1,11 @@
 import book_recommender
 
-def test_book_build():
+def test_book_build(self):
+    '''books get put into a list'''
     pass
 
-def test_recommender_build():
+def test_recommender_build(self):
+    '''What will be tested author, rating, length
+    top books, starting index, length for short medium and long'''
     pass
     
