0ded5ab5723de5ff366ef0659e84c82af68bbe5f
Travious46 <46880742+Travious46@users.noreply.github.com>
2022-05-12 21:48:41 -0400

    deleted main and parse_args function

--- book_recommender.py
+++ book_recommender.py
@@ -11,8 +11,6 @@ import sys
 import argparse
 import csv
 
-from numpy import size
-
 """ A class for holding responses from the user about their book preferences.
         Attributes:
             preferred_author (String): User response to preferred author question
@@ -29,14 +27,16 @@ class Recommender:
             next(spreadsheet)
             
             for row in spreadsheet:
-                self.books.append(Book(row[1],row[2],row[3],row[7]))
+                self.books.append(Book(row[1],row[2],row[3], row[7]))
+                print(row[7])
     
     def recommend(self, author, avg_rating, length):
         top_books = list()
         index = 0
-        while size(top_books) < 5:
+        while len(top_books) < 5:
             book = self.books[index]
-            if book.author == author and book.avg_rating >= avg_rating and book.length >= length:
+            print(self.books[5].length)
+            if book.author == author and book.avg_rating >= avg_rating and book.length == length:
                 top_books.append(book)
             index += 1
             
@@ -53,41 +53,26 @@ class Book:
         length (string): The length of a book either being 'short' or 'long' based on page count.
     """
     def __init__(self, title, author, avg_rating, length):
-        #See class documentation
         self.title = title
         self.author = author
-        #self.genre = genre
         self.avg_rating = avg_rating
-        self.length = length
+        #The length is 'short' if the page count is <= 300, 'medium' if page count is > 300 and <= 600,
+        #'long' if page count is larger.
+        self.length = int(length)
+        if self.length <= 300:
+            self.length = 'short'
+        elif self.length > 300 and self.length <= 600:
+                self.length = 'medium'
+        else:
+            self.length = 'long'
             
-def main(parameter1, parameter2):
+if __name__ == "__main__":
     preferred_author = input("Please enter if you have a preference for a certain author: ")
-    preferred_rating = input("Pleaes enter if you have a preference for a certain rating range (1-5): ")
-    preferred_length = input("Please enter if you have a preference for a certain length (short, medium, long): ")
+    preferred_rating = input("Please enter if you have a preference for a certain rating range (1-5): ")
+    preferred_length = input("Please enter if you have a preference for a certain length: ")
     new_Recommender = Recommender()
     recommended_books = new_Recommender.recommend(preferred_author,preferred_rating,preferred_length)
     for book in recommended_books:
         print(book.title)
-    
-def parse_args(args_list):
-    """Takes a list of strings from the command prompt and passes them through as arguments
-    
-    Args:
-        args_list (list) : the list of strings from the command prompt
-    Returns:
-        args (ArgumentParser)
-    """
-    
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('--optional', type=float, help='This is an example of a required argument.')
-    parser.add_argument('--optional', '-o', type=int, default=12, help='This is an example of an optional argument')  
-    
-    args = parser.parse_args(args_list)
-
-    return args
-
-if __name__ == "__main__":
-    main(None, None)
 
 
