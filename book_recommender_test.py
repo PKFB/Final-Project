1b35037ffb87ffbe16f755f10babd2d38f6dd0e7
melissakavege <mkavege53@gmail.com>
2022-05-13 10:34:30 -0400

    Update book_recommender_test.py

--- book_recommender_test.py
+++ book_recommender_test.py
@@ -1,8 +1,28 @@
 import book_recommender
 
-def test_book_build():
-    pass
+from book_recommender import book
+from book_recommender import Recommender
 
-def test_recommender_build():
-    pass
-    
+
+    def test_book_build(self):
+        book()
+   ''books get put into a list'''
+   #happy path
+       self
+     
+  #edge case
+
+    def test_recommender_build(self):
+    Recommender
+    '''What will be tested author, rating, length
+    top books, starting index, length for short medium and long'''
+   #happy path
+        self.assertRecommender
+     
+  #edge case
+        
+        
+        
+if __name__ == "__main__":
+    test_book_build()
+    test_recommender_build()
